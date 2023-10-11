02ab26a7b268453573ed6270287a5836cc94bbc5
Seth Banta <sethbanta@u.boisestate.edu>
2023-10-11 11:44:37 -0600

    Create ToDoList.py
    Created a python file named ToDoList.py this will contain the code for the CLI. Eventually it will support creating, reading, updating, and deleting tasks. Sorting by priority, due date, and title. As well as the ability to export to file, and import from file

---
+++ ToDoList.py
@@ -0,0 +1,21 @@
+#Author Seth Banta
+
+#Define task object
+
+#create tasks
+
+#read tasks
+
+#update tasks
+
+#delete tasks
+
+#sort by priority: high, low
+
+#sort by due date: closest, furthest
+
+#sort by title alphabetically
+
+#save tasks to file
+
+#import tasks from file
