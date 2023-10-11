75b75746e9925706edcc164986d5780dd61ea84c
Seth Banta <sethbanta@u.boisestate.edu>
2023-10-11 12:00:01 -0600

    Create task object
    Created a new class called Task that allows objects to be created as tasks containing a title, description, priority, and due date.

--- ToDoList.py
+++ ToDoList.py
@@ -1,7 +1,16 @@
 #Author Seth Banta
 
 #Define task object
-
+class Task:
+    def __init__(self, title, description, priority, due_date):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.due_date = due_date
+        
+    def __str__(self):
+        return f"{self.title}, {self.description}, {self.priority}, {self.due_date}"
+        
 #create tasks
 
 #read tasks
